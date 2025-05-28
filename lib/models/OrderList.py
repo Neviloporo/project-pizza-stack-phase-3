b4c3849b30415543a0a598d077d16ab5edd79f07
nevil <nevil.oporo@student.moringaschool.com>
2025-05-28 22:14:42 +0300

    Made the OrderList

--- lib/models/OrderList.py
+++ lib/models/OrderList.py
@@ -1,4 +1,25 @@
 from Order import Order
 
 class OrderList:
-    pass
+    
+    def list_all_orders():
+        return Order.list_all_orders()
+    
+    def list_pending_orders():
+        return [order for order in Order.List_all_orders() if not order.completed]
+    
+    def list_completed_orders():
+        return [order for order in Order.list_all_order() if order.completed]
+    
+    def list_orders_by_user(user_id):
+        return Order.list_orders_by_user(user_id)
+    
+    def mark_order_completed(order_id):
+        order = Order.get_order_by_id(order_id)
+        if order:
+            order.mark_completed()
+            return True
+        return False
+    
+    def get_order_by_id(order_id):
+        return Order.get_order_by_id(order_id)
